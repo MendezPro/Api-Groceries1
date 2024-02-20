8b98b75f4578e2cdbf7cbed0b22d274d7854ec86
Orlando Méndez Montes <150469470+MendezPro@users.noreply.github.com>
2024-02-20 17:13:57 -0600

    Update products.controllers.js

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,49 +1,72 @@
-import productDAO from "../dao/products.dao.js";
+import ProductDAO from "../dao/products.dao.js";
 
-const getAll = (req,res) =>{
-    productDAO.getAll()
-    .then(products=>{
-        !products ? res.json({Status:"No data to show"}): res.json({products})
-    })
-    .catch(err=>res.json({
-        status:"Server unavailable"
-    }))
-}
-const getOne = (req,res) =>{
-    const barcode= req.params.barcode
-    productDAO.getOne(barcode)
-    .then(result=>{
-        !result?res.json({Status:`Barcode "${barcode}" Not Found :,(`}):res.json({Product:result})
-    })
-    .catch(err=>res.json({
-        status:"Server unavailable"
-    }))
-}
-const insertOne = (req,res) =>{
-    const product= req.body
-    productDAO.insertOne(product)
-    .then(result=>{
-        res.json({Status:"Product inserted correctly"})
-    })
-    .catch(err=>console.log("ño"))
-}
-const updateOne = (req,res) =>{
-    productDAO.updateOne(req.params.barcode,req.body)
-    .then(result=>{
-        !result?res.json({Status:`Barcode "${req.params.barcode}" Not Found :,(`}):res.json({estatus:"Data update sucessfully"})
-    })
-    .catch(err=>res.json({
-        status:"Server unavailable"
-    }))
-}
-const deleteOne = (req,res) =>{
-    productDAO.deleteOne(req.params.barcode)
-    .then(result=>{
-        !result?res.json({Status:`Barcode "${req.params.barcode}" Not Found :,(`}):res.json({estatus:"Data was delete sucesfully"})
-    })
-    .catch(err=>res.json({
-        status:"Server unavailable"
-    }))
+const productsController = {};
+
+export const getAll = (req, res) => {
+    ProductDAO.getAll()
+        .then(products => {
+            res.render('../src/views/index.ejs', { products }); // Aquí se pasa un objeto con la propiedad 'products'
+        })
+        .catch(err => res.json({
+            status: "Server unavailable"
+        }));
 }
 
-export {getAll,getOne,insertOne, updateOne, deleteOne}
+export const getOne = (req, res) => {
+    const barcode = req.params.barcode;
+    ProductDAO.getOne(barcode)
+        .then(product => {
+            if (product) {
+                res.render("../src/views/edit.ejs", {  product   });
+            } else {
+                res.json({
+                    status: "Product not found"
+                });
+            }
+        })
+        .catch(err => res.json({
+            status: "Server unavailable"
+        }));
+}
+
+export const insertOne = (req, res) => {
+    ProductDAO.insertOne(req.body)
+        .then(result => res.redirect('/'))
+        .catch(err => res.json({
+            status: "Server unavailable"
+        }));
+}
+
+export const updateOne = (req, res) => {
+    const barcode = req.params.barcode;
+    const product = req.body;
+    ProductDAO.updateOne(barcode, product)
+        .then(result => {
+            if (result) {
+                res.redirect("/");
+            } else {
+                res.json({
+                    status: "Product not found"
+                });
+            }
+        })
+        .catch(err => res.json({
+            status: "Server unavailable"
+        }));
+}
+
+export const deleteOne = (req, res) => {
+    const barcode = req.params.barcode;
+    ProductDAO.deleteOne(barcode)
+      .then(result => {
+        if (result) {
+            res.redirect("/");
+    } else {
+        res.json({
+            status: "Product not found"
+        });
+    }})
+      .catch(err => res.json({
+            status: "Server unavailable"
+        }));
+};
